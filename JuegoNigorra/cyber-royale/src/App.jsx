c19d4beffb3d9959326a9e4909b1563cc24dc396
marcoosruiz90-cmyk <marcoosruiz90@gmail.com>
2026-05-19 17:36:52 +0200

    AddAlertErrorLobby

--- JuegoNigorra/cyber-royale/src/App.jsx
+++ JuegoNigorra/cyber-royale/src/App.jsx
@@ -383,6 +383,7 @@ export default function App() {
       setView('game');
     } catch (err) {
       console.error(err);
+      alert('⚠️ Error de Base de Datos al iniciar partida: ' + err.message);
     }
   };
 
